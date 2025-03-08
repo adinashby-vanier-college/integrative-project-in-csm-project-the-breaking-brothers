7f7fb9da40b166b5654688db065648e8918d5fd6
Marco Boi <2359284@edu.vaniercollege.qc.ca>
2025-03-08 17:08:43 -0500

    Update HelloApplication.java

--- Deliverable 3/Chemistry/src/main/java/com/example/chemistry/HelloApplication.java
+++ Deliverable 3/Chemistry/src/main/java/com/example/chemistry/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println("hello! how are u");
+        System.out.println("hello! how are u, good, great");
     }
 
     public static void main(String[] args) {
