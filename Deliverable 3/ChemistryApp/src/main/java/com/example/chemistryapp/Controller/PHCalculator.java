24d67230dd356561ff8fbc53e78916f0778c2efa
Sahon Shaha <shahasahon@gmail.com>
2025-05-04 20:06:47 -0400

    Pushing again cuz last time not everything got pushed

--- Deliverable 3/ChemistryApp/src/main/java/com/example/chemistryapp/Controller/PHCalculator.java
+++ Deliverable 3/ChemistryApp/src/main/java/com/example/chemistryapp/Controller/PHCalculator.java
@@ -8,4 +8,19 @@ public class PHCalculator {
     public double convertTopOH(double pH) {
         return 14 - pH;
     }
+
+    public double H30ConcentrationTopH(double concentration) {
+        return -(Math.log(concentration));
+    }
+
+    public double pHToH30Concentration(double pH) {
+        return Math.pow(10, -pH);
+    }
+
+    public double pOHToOHConcentration(double pOH) {
+        return Math.pow(10, -pOH);
+    }
+
+    // Fidning pH of a subsatnce: Input: concentration (mol/L) Output: pH, Formula: -log[CONCETRATION]
+    // pH to Concetration: Formula: 10^-pH
 }
