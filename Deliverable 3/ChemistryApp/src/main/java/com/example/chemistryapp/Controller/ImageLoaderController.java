3deebab95358d7d9967c641cfd2759846b8c2870
Marco Boi <2359284@edu.vaniercollege.qc.ca>
2025-05-04 20:03:21 -0400

    Update ImageLoaderController.java

--- Deliverable 3/ChemistryApp/src/main/java/com/example/chemistryapp/Controller/ImageLoaderController.java
+++ Deliverable 3/ChemistryApp/src/main/java/com/example/chemistryapp/Controller/ImageLoaderController.java
@@ -44,7 +44,7 @@ public class ImageLoaderController {
         }
     }
 
-    public static void loadMoleculeImage(String moleculeFormula, Consumer<Image> callback) {
+    public void loadMoleculeImage(String moleculeFormula, Consumer<Image> callback) {
         String urlString = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/" + moleculeFormula + "/PNG";
 
         new Thread(() -> {
