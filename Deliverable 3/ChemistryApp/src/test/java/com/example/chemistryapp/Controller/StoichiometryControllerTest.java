f98c6894a1f1b02e34b286ef20aa12aff29d6bf2
Sahon Shaha <shahasahon@gmail.com>
2025-05-04 20:04:23 -0400

    Finalized equation balancing system, added extra molecule slots to balance, added more calculators for PHCalculator

---
+++ Deliverable 3/ChemistryApp/src/test/java/com/example/chemistryapp/Controller/StoichiometryControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.chemistryapp.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoichiometryControllerTest {
+
+    @Test
+    void getElementSubscripts() {
+
+    }
+
+    @Test
+    void getUniqueElements() {
+    }
+
+    @Test
+    void createMatrix() {
+    }
+
+    @Test
+    void getElementSymbols() {
+    }
+
+    @Test
+    void rref() {
+    }
+
+    @Test
+    void gcd() {
+    }
+
+    @Test
+    void lcm() {
+    }
+
+    @Test
+    void getBalancedEquation() {
+    }
+
+    @Test
+    void missingFieldCalculator() {
+    }
+}
